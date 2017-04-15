2c080e0440553f74a6e518d5d099bdaeaa70ac38
Chris Black <chris@ckblack.org>
2017-04-14 22:57:42 -0400

    some tests

---
+++ tests/testthat/test_booleans.R
@@ -0,0 +1,9 @@
+context("If these don't work we are in truh-bull")
+
+test_that("TRUE is true", {
+	expect_true(TRUE)
+})
+
+test_that("FALSE is false",{
+	expect_error(stop("this isn't false at all!"))
+})
