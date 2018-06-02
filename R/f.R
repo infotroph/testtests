619169bac1599a1fefdcb390914dd115cadfaab6
Chris Black <chris@ckblack.org>
2018-06-02 12:51:19 +0200

    dunno what I was in the middle of last time but lets see if it passes

--- R/f.R
+++ R/f.R
@@ -6,3 +6,27 @@
 #' @return the output
 #' @export
 f <- function(x){x}
+
+#' test if `::` on packages in Suggests causes R CMD check warning
+#'
+#' @param ... arguments, you know
+#' @export
+conditional <- function(...){
+	if(requireNamespace("plotrix", quietly=TRUE)){
+		return(plotrix::std.error(...))
+	}else{
+		print("you're missing out, but whatever")
+		return(3)
+	}
+	
+}
+
+#' a function that forgets a namespace call
+#'
+#' uses dplyr::select but doesn't call it that
+#'
+#' @param df a dataframe
+#' @export
+g <- function(df){
+	select(df, -1) 
+}
--- tests/testthat/test_booleans.R
+++ tests/testthat/test_booleans.R
@@ -1,13 +1,44 @@
-context("If these don't work we are in truh-bull")
+# context("If these don't work we are in truh-bull")
 
-teardown(
-	print(packageVersion("testthat"))
-)
+# # skip_if_not_installed("foobar")
 
-test_that("TRUE is true", {
-	expect_true(TRUE)
+# test_that("TRUE is true", {
+# 	expect_true(TRUE)
+# })
+
+# # test_that("FALSE is false",{
+# # 	expect_error(stop("this isn't false at all!"))
+# # })
+
+# # # library(dplyr)
+
+# # test_that("g 'works'", {
+# # 	expect_equal(mtcars[,-1], g(mtcars))
+# # })
+
+# context("something else")
+
+# test_that("printing prints",{
+# 	expect_output(print(3))
+# })
+
+context("Optional bits")
+
+test_that("optional features play nice with somePkg",{
+	# skip_if_not_installed("somePkg")
+
+	test_that("nested tests?", {
+		expect_true(FALSE)
+	})
+	
+	test_that("nested! tests!", {
+		skip("broken even when somePkg installed")
+		expect_equal(somePkg::three(), two()+1)
+	})
+	
+	expect_equal(1,2)
 })
 
-test_that("FALSE is false",{
-	expect_error(stop("this isn't false at all!"))
+test_that("this test not nested", {
+	expect_false(TRUE)
 })
